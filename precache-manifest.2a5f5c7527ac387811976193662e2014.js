4d0b48896bf5827a06ef38f67f619a424a4991c8
vishalrranjan <vishalkr275@gmail.com>
2019-10-31 09:42:50 +0530

    Updates

---
+++ precache-manifest.2a5f5c7527ac387811976193662e2014.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59fe82a5f9c9b9ba0bc5fb4dbf34577",
+    "url": "/robotcard/index.html"
+  },
+  {
+    "revision": "b8a8284859fa3388b840",
+    "url": "/robotcard/static/css/main.fb02354a.chunk.css"
+  },
+  {
+    "revision": "6d142608e4518611bc4e",
+    "url": "/robotcard/static/js/2.d8d3da0b.chunk.js"
+  },
+  {
+    "revision": "b8a8284859fa3388b840",
+    "url": "/robotcard/static/js/main.ff4f5b20.chunk.js"
+  },
+  {
+    "revision": "59aee759a236fcd8cdb2",
+    "url": "/robotcard/static/js/runtime-main.fc4ea57a.js"
+  }
+]);
